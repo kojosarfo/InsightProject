d5e4c9c9e4123e3b6d4e4a3ae6e057446cfaa482
kojosarfo <askgyamfi@gmail.com>
2019-10-03 10:21:24 -0400

    Updated scripts for training

--- scripts/trainGPR.py
+++ scripts/trainGPR.py
@@ -39,7 +39,13 @@ for i in range(2):
     
     trafficData = trafficData.dropna()
     
-    X = trafficData[['Hour','DayOfWeek','isHoliday','lat','lng']]; #Feature selection showed these features to be most predictive of the traffic volumes. Perhaps no weather parameter significantly influences pedestrian/cyclist traffic because the weather doesn't vary much between May and August.
+    #X = trafficData[['Hour','DayOfWeek','isHoliday','lat','lng']]; 
+    #Feature selection showed these features to be most predictive of 
+    #the traffic volumes. Perhaps no weather parameter significantly 
+    #influences pedestrian/cyclist traffic because the weather doesn't 
+    #vary much between May and August.
+    
+    X = trafficData[['Hour','DayOfWeek','isHoliday','lat','lng',]];
     
     Y = trafficData['volume']
     
@@ -47,7 +53,11 @@ for i in range(2):
     X_train, X_test, y_train, y_test = \
     train_test_split(X, Y, test_size=0.2, random_state=0)
     
-    #Training with Gaussian process regression. The kernel used is a radial basis function (squared exponential) kernel whose length_scale is initialised to a vector of all ones: [1,1,1,1,1], the size of which is equal to the number of features (5);
+    #Training with Gaussian process regression. The kernel used is a 
+    #radial basis function (squared exponential) kernel whose 
+    #length_scale is initialised to a vector of all ones: [1,1,1,1,1], 
+    #the size of which is equal to the number of features (5);
+    
     kernel = RBF(length_scale=np.ones([5,]))
     gprMdl = gpr(kernel=kernel, random_state=0, normalize_y=True, 
                  n_restarts_optimizer = 0).fit(X_train, y_train)
@@ -57,13 +67,18 @@ for i in range(2):
         pickle.dump(gprMdl, handle, protocol=pickle.HIGHEST_PROTOCOL)
                
     # Validation
-    yPred, yStd = gprMdl.predict(X_test, return_std=True) #Gaussian process regression returns the standard devition of the prediction
-    yPred = yPred.astype('int'); yPred[yPred<0]=0 #Since we are forecasting pedestrian traffic, we round of all floats and trim negative values to zero.
+    yPred, yStd = gprMdl.predict(X_test, return_std=True) 
+    #Gaussian process regression returns the standard devition 
+    #of the prediction
+    yPred = yPred.astype('int'); yPred[yPred<0]=0 
+    #Since we are forecasting pedestrian traffic, we round of all 
+    #floats and trim negative values to zero.
     
     yPred = pd.DataFrame(yPred, columns = ['forecast']); 
     yPred.index = y_test.index; yPred = yPred['forecast'];
     
-    # +/- 3 times the standard deviation is used, representing a 99.7% confidence bound
+    # +/- 3 times the standard deviation is used, representing a 99.7% 
+    #confidence bound
     yLow = yPred - 3*yStd; yLow = yLow.astype('int'); yLow[yLow<0]=0
     yHigh = yPred + 3*yStd; yHigh = yHigh.astype('int');
     
